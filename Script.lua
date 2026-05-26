ab5422e318782d7ee27be5afd62f7829fd6115f1
Webmaker669 <ethynh2010@gmail.com>
2026-05-26 16:23:16 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -505,7 +505,7 @@ local RunService = uiData.RunService
 local LocalPlayer = uiData.LocalPlayer
 
 local isSelecting = false
-local blockName = "GoldBlock"
+local blockName = "PlasticBlock"
 
 btnSelect.MouseButton1Click:Connect(function()
 	if isSelecting then return end
